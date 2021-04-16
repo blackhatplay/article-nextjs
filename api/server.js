27f789c20969ac312a937ee044136687010a435c
blackhatplay <mannuall4u@gmail.com>
2021-04-16 07:48:33 +0530

    server

--- api/server.js
+++ api/server.js
@@ -3,6 +3,5 @@ import axios from "axios";
 export const serverURL = process.env.NEXT_PUBLIC_SERVER;
 
 export default axios.create({
-  // baseURL: serverURL,
-  baseURL: `https://articles-backend.glitch.me`,
+  baseURL: serverURL,
 });
