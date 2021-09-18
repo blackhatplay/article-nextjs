32ef7ecff3cc82d307fc667c78de305d26e7c9a1
Manoj Singh <ext.manoj.s@goodworker.in>
2021-09-18 07:47:49 +0530

    generated new package lock file

--- utils/theme.js
+++ utils/theme.js
@@ -1,36 +1,36 @@
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { createTheme } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#252530",
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#252530',
+        },
+        secondary: {
+            main: '#fafafa',
+        },
+        background: {
+            default: '#252530',
+        },
+        text: {
+            primary: '#fff',
+            secondary: '#f4f4f4',
+        },
     },
-    secondary: {
-      main: "#fafafa",
+    typography: {
+        fontFamily: [
+            '"Poppins"',
+            'Roboto',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
     },
-    background: {
-      default: "#252530",
-    },
-    text: {
-      primary: "#fff",
-      secondary: "#f4f4f4",
-    },
-  },
-  typography: {
-    fontFamily: [
-      '"Poppins"',
-      "Roboto",
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-  },
 });
 
 export default theme;
